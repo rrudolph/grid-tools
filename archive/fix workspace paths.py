ba36769a5ca6c4f0d8b6328dc4f2deabde0c1ae9
Rudolph <RRudolph@nps.gov>
2020-05-13 14:51:52 -0700

    removed archive

--- archive/fix workspace paths.py
+++
@@ -1,8 +0,0 @@
-
-import arcpy
-mxd = arcpy.mapping.MapDocument("CURRENT")
-mxd.findAndReplaceWorkspacePaths(r"C:\Temp\SantaCruzIsland_WeedTreatment_20200427\NChannelIslandsTreatmentTemplate.gdb",
-	r"C:\Temp\SantaCruzIsland_WeedTreatment_20200427\CHIS_Invasive_Treatments.gdb")
-mxd.saveACopy(r"C:\Temp\SantaCruzIsland_WeedTreatment_20200427\Nad83.mxd")
-del mxd
-
